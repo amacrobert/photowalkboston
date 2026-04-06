908b09e932c878ef9123c54fc1d844085555a595
Andrew MacRobert <andrew.macrobert@gmail.com>
2026-04-06 08:23:12 -0400

    Disable email notifications

--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -18,26 +18,26 @@ class EmailService
 
     public function sendApplicationEmail(Application $application): void
     {
-        $recipients = $this->recipientRepo->findActive();
-
-        $toAddresses = array_map(
-            fn($recipient) => new Address($recipient->getEmail(), $recipient->getName()),
-            $recipients
-        );
-
-        if (empty($toAddresses)) {
-            // Don't bother sending an email to nobody
-            return;
-        }
-
-        $email = (new TemplatedEmail())
-            ->from(new Address('apply@photowalkboston.com', 'Photo Walk Boston Applications'))
-            ->to(...$toAddresses)
-            ->subject(sprintf("%s's request to join Photo Walk Boston", $application->getName()))
-            ->replyTo(new Address($application->getEmail(), $application->getName()))
-            ->htmlTemplate('email/application.html.twig')
-            ->context(['application' => $application]);
-
-        $this->mailer->send($email);
+        // $recipients = $this->recipientRepo->findActive();
+        //
+        // $toAddresses = array_map(
+        //     fn($recipient) => new Address($recipient->getEmail(), $recipient->getName()),
+        //     $recipients
+        // );
+        //
+        // if (empty($toAddresses)) {
+        //     // Don't bother sending an email to nobody
+        //     return;
+        // }
+        //
+        // $email = (new TemplatedEmail())
+        //     ->from(new Address('apply@photowalkboston.com', 'Photo Walk Boston Applications'))
+        //     ->to(...$toAddresses)
+        //     ->subject(sprintf("%s's request to join Photo Walk Boston", $application->getName()))
+        //     ->replyTo(new Address($application->getEmail(), $application->getName()))
+        //     ->htmlTemplate('email/application.html.twig')
+        //     ->context(['application' => $application]);
+        //
+        // $this->mailer->send($email);
     }
 }
