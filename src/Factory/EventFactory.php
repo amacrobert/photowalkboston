8d4edb63af29695789624a1ef030e66fae8e995e
Andrew MacRobert <andrew.macrobert@gmail.com>
2026-04-10 12:30:31 -0400

    Event and Image factories

---
+++ src/Factory/EventFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Event;
+use Zenstruck\Foundry\Persistence\PersistentObjectFactory;
+
+/**
+ * @extends PersistentObjectFactory<Event>
+ */
+final class EventFactory extends PersistentObjectFactory
+{
+    #[\Override]
+    public static function class(): string
+    {
+        return Event::class;
+    }
+
+    #[\Override]
+    protected function defaults(): array
+    {
+        return [
+            'date' => self::faker()->dateTimeBetween('now', '+30 days'),
+            'title' => self::faker()->text(),
+        ];
+    }
+}
---
+++ src/Factory/ImageFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Image;
+use Zenstruck\Foundry\Persistence\PersistentObjectFactory;
+
+/**
+ * @extends PersistentObjectFactory<Image>
+ */
+final class ImageFactory extends PersistentObjectFactory
+{
+    #[\Override]
+    public static function class(): string
+    {
+        return Image::class;
+    }
+
+    #[\Override]
+    protected function defaults(): array
+    {
+        return [
+            'filename' => '/images/' . self::faker()->word() . '.jpg',
+            'credit' => self::faker()->name(),
+            'updated' => self::faker()->dateTime(),
+        ];
+    }
+}
